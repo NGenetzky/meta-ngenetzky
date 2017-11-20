e9f4ddf80056a4e316893e07ff7e3188729d18e7
Nathan Genetzky <nathan@genetzky.us>
2017-11-20 00:18:45 -0600

    simple-server-python: Simple server using Flask.

---
+++ recipes-python/simple-server-python/simple-server-python/setup.py
@@ -0,0 +1,12 @@
+import os
+from setuptools import setup
+
+setup(
+    name = "simple_server_python",
+    version = "0.0.1",
+    author = "",
+    author_email = "",
+    description = ("foo"),
+    license = "MIT",
+    packages=['simple_server_python'],
+)
