75818b10f269c78e846c47bdc5049001783bacf3
Nathan Genetzky <nathan@genetzky.us>
2018-06-20 22:34:04 -0500

    ngenetzky_base: Source script to init build env

---
+++ scripts/ngenetzky_base.bash
@@ -0,0 +1,49 @@
+#!/bin/bash
+GITROOT="${GITROOT-$(readlink -f ./$(git rev-parse --show-cdup))}"
+BITBAKE_DIR="${GITROOT}/../bitbake"
+BUILD_DIR="${GITROOT}/../build"
+
+bitbake_clone(){
+    local dest="${BITBAKE_DIR}"
+    local remote="https://github.com/openembedded/bitbake"
+    if [ ! -d ${dest} ] ; then
+      git clone "${remote}" "${dest}"
+    fi
+}
+
+bitbake_conf() {
+  local name='ngenetzky-base'
+  local confdir="${BUILD_DIR?}/conf"
+  mkdir -p "${confdir}"
+  install -m 664 --target-directory="${confdir}" \
+      "${GITROOT}/projects/${name}/local.conf" \
+      "${GITROOT}/projects/${name}/bblayers.conf"
+}
+
+bitbake_set_path(){
+  local p="$(readlink -f ${1?})"
+    [[ -d ${p} ]] || return 1
+    export PATH="${p}/bin:$PATH"
+    export PYTHONPATH="${p}/lib:$PYTHONPATH"
+}
+
+main(){
+  bitbake_clone
+  bitbake_conf
+}
+
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+    # Bash Strict Mode
+    set -eu -o pipefail
+
+    set -x
+    main "$@"
+else
+  main
+  bitbake_set_path "${BITBAKE_DIR}"
+  cd "${BUILD_DIR}"
+
+  unset bitbake_clone
+  unset bitbake_conf
+  unset bitbake_set_path
+fi
